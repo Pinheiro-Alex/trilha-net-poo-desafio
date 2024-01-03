97c4697199adcea6a6d83c53106132528fbb826b
Alex Pinheiro <alexpinheiro008@gmail.com>
2024-01-02 23:18:07 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO.Models 
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia: ");
+            Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo K", imei: "121111111", memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatSapp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone Iphone: ");
+            Smartphone iphone = new Iphone(numero: "789456", modelo: "K9", imei: "455454455", memoria: 120);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
+
+
